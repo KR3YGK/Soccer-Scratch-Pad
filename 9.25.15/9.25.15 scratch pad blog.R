b88fc9ddc12c7cdae49f0385c2114a72891d24a2
KR3YGK <econkerr@gmaill.com>
2016-03-12 16:15:52 -0800

    updated sd of ideal league to be sqrt(14/(81p))

--- 9.25.15/9.25.15 scratch pad blog.R
+++ 9.25.15/9.25.15 scratch pad blog.R
@@ -6,6 +6,7 @@
 #                                    #
 #written by Craig Kerr               #
 #cakerr@cpp.edu                      #
+#updated 3.12.16                     #
 ######################################
 
 #change the working directory to wherever you put mls standings 1996-2013.csv
@@ -16,7 +17,7 @@ data<-read.csv("mls standings 1996-2013.csv")
 data_gp32<-subset(data,year==2005 | year==2006 | year<=2000)
 
 x   <- seq(0,1,length=1000)
-y   <- dnorm(x,mean=(4/9), sd=sqrt(2/(9*32)))
+y   <- dnorm(x,mean=(4/9), sd=sqrt(14/(81*32)))
 
 #use hypothetical pct b/c shootout era included
 plot(density(data_gp32$hypothetical_pct, bw=.01),col="blue",
@@ -24,13 +25,13 @@ plot(density(data_gp32$hypothetical_pct, bw=.01),col="blue",
 lines(x,y, type="l", lwd=2)
 
 #kolmogorov-smirnov test that dist are diff
-ks.test(data_gp32$hypothetical_pct,"pnorm",mean=4/9,sd=sqrt(2/(9*32)))
+ks.test(data_gp32$hypothetical_pct,"pnorm",mean=4/9,sd=sqrt(14/(81*32)))
 
 #subset 2003,2004, 2007-2010 all gp=30
 data_gp30<-subset(data,year==2003 | year==2004 | year>=2007 & year<=2010)
 
 x   <- seq(0,1,length=1000)
-y   <- dnorm(x,mean=(4/9), sd=sqrt(2/(9*30)))
+y   <- dnorm(x,mean=(4/9), sd=sqrt(14/(81*30)))
 
 
 plot(density(data_gp30$pct, bw=.01),col="blue",
@@ -38,14 +39,14 @@ plot(density(data_gp30$pct, bw=.01),col="blue",
 lines(x,y, type="l", lwd=2)
 
 #kolmogorov-smirnov test that dist are diff
-ks.test(data_gp30$pct,"pnorm",mean=4/9,sd=sqrt(2/(9*30)))
+ks.test(data_gp30$pct,"pnorm",mean=4/9,sd=sqrt(14/(81*30)))
 
 
 #subset 2011-2014 all gp=34
 data_gp34<-subset(data,year>2010)
 
 x   <- seq(0,1,length=1000)
-y   <- dnorm(x,mean=(4/9), sd=sqrt(2/(9*34)))
+y   <- dnorm(x,mean=(4/9), sd=sqrt(14/(81*34)))
 
 
 plot(density(data_gp34$pct, bw=.01),col="blue",
@@ -53,5 +54,5 @@ plot(density(data_gp34$pct, bw=.01),col="blue",
 lines(x,y, type="l", lwd=2)
 
 #kolmogorov-smirnov test that dist are diff
-ks.test(data_gp34$pct,"pnorm",mean=4/9,sd=sqrt(2/(9*34)))
+ks.test(data_gp34$pct,"pnorm",mean=4/9,sd=sqrt(14/(81*34)))
 
