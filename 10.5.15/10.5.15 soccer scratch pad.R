ed08df19a6de6034647bc1c8bcc87fb7e5a981c2
KR3YGK <econkerr@gmaill.com>
2016-11-17 23:22:57 -0800

    added graph
    Added percentH.jpeg to reference.  Made with R.  Need to maybe weigh by
    minute, not game.

--- 10.5.15/10.5.15 soccer scratch pad.R
+++ 10.5.15/10.5.15 soccer scratch pad.R
@@ -1,8 +1,5 @@
 ######################################
 #data obtained from mlssoccer.com    # 
-#plots %points earned for all        #
-#team-seasons with the same #of games#
-#and compares to ideal distribution  #
 #                                    #
 #written by Craig Kerr               #
 #cakerr@cpp.edu                      #
@@ -16,8 +13,8 @@ attach(data)
 
 
 #for the plot without the lines, exlude the "abline" lines
-plot(year,pct_tie,main="% Games Tied in Regulation",ylab="% tied",pch=1,col=4,xaxt="n")
+plot(year,pct_tie_reg,main="% Games Tied in Regulation",ylab="% tied",pch=1,col=4,xaxt="n")
 axis(1, at = seq(1996, 2014, by = 1),las=2)
-#abline(v=1999.5,col="red")
-#abline(v=2002.5,col="red")
+abline(v=1999.5,col="red")
+abline(v=2002.5,col="red")
 detach(data)
